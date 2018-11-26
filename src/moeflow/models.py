d8545605f57e9baa20a06b73d3c7257c09cebe07
rachmadaniHaryono <foreturiga@gmail.com>
2018-11-26 16:19:06 +0800

    chg: dev: complete model

--- src/moeflow/models.py
+++ src/moeflow/models.py
@@ -1,64 +1,89 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Table, Float
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils import ColorType
 from sqlalchemy_utils.models import Timestamp
 
 
 Base = declarative_base()
-checksum_tags = Table('checksum_tags', Base.metadata,
+checksum_tags = Table(
+    'checksum_tags', Base.metadata,
     Column('checksum_id', ForeignKey('checksum.id'), primary_key=True),
     Column('tag_id', ForeignKey('checksum.id'), primary_key=True)
 )
+checksum_faces = Table(
+    'checksum_faces', Base.metadata,
+    Column('checksum_id', ForeignKey('checksum.id'), primary_key=True),
+    Column('face_id', ForeignKey('face.id'), primary_key=True)
+)
+face_color_tags = Table(
+    'face_color_Tags', Base.metadata,
+    Column('face_id', ForeignKey('face.id'), primary_key=True),
+    Column('color_tag_id', ForeignKey('color_tag.id'), primary_key=True)
+)
+face_tags = Table(
+    'face_tags', Base.metadata,
+    Column('face_id', ForeignKey('face.id'), primary_key=True),
+    Column('tag_id', ForeignKey('tag.id'), primary_key=True)
+)
+face_prediction_tags = Table(
+    'face_prediction_tags', Base.metadata,
+    Column('face_prediction_id', ForeignKey('face_prediction.id'), primary_key=True),
+    Column('tag_id', ForeignKey('tag.id'), primary_key=True)
+)
 
 
-class Checksum(Base, Timestamp):
+class BaseModel(Base, Timestamp):
+    __abstract__ = True
     id = Column(Integer, primary_key=True)
+
+
+class Checksum(BaseModel):
     value = Column(String, unique=True)
     ext = Column(String)
-    trash = Column(Boolean, default=True)
+    trash = Column(Boolean, default=False)
     width = Column(Integer)
     height = Column(Integer)
-    tags = Tag
-    #  faces = Face
+    tags = relationship('Tag', secondary=checksum_tags, back_populates='checksums')
 
 
-class Face(Base, Timestamp):
-    id = Column(Integer, primary_key=True)
-    checksum_id
-    checksum
-    resized_checksum_id
-    resized_checksum
-    method
+class Face(BaseModel):
+    checksum_id = Column(Integer, ForeignKey('checksum.id'))
+    checksum = relationship("Checksum", back_populates="faces", foreign_keys=checksum_id)
+    resized_checksum_id = Column(Integer, ForeignKey('checksum.id'))
+    resized_checksum = relationship(
+        "Checksum", back_populates="faces", foreign_keys=resized_checksum_id)
+    method = Column(String)
     pos_x = Column(Integer)
     pos_y = Column(Integer)
     pos_w = Column(Integer)
     pos_h = Column(Integer)
-    colors = ColorTag
-    tags = Tag
-    prediction = FacePrediction
+    colors = relationship('ColorTag', secondary=face_color_tags, back_populates='faces')
+    tags = relationship('Tag', secondary=face_tags, back_populates='faces')
 
 
-class FacePrediction(Base, Timestamp):
-    face_id
-    face
-    method
-    tag
-    confidence
+class FacePrediction(BaseModel):
+    face_id = Column(Integer, ForeignKey('face.id'))
+    face = relationship("Face", back_populates="predictions", foreign_keys=face_id)
+    method = Column(String)
+    tags = relationship('Tag', secondary=face_tags, back_populates='face_predictions')
+    confidence = Column(Float)
 
 
-
-class Tag(Base):
+class Tag(BaseModel):
     value = Column(String)
-    namespace_id
-    namespace
-    parents
-    sibling
+    namespace_id = Column(Integer, ForeignKey('namespace.id'))
+    namespace = relationship(
+        "Namespace", back_populates="tags", foreign_keys=namespace_id)
+    parents = relationship("Tag", backref=backref('children', remote_side=[id]))
+    sibling_id = Column(Integer, ForeignKey('tag.id'))
+    sibling = relationship("Tag", back_populates="siblings", foreign_keys=sibling_id)
 
 
-class Namespace(Base):
+class Namespace(BaseModel):
     value = Column(String)
 
 
-class ColorTag(Base):
+class ColorTag(BaseModel):
     value = Column(String)
     color_value = Column(ColorType)
